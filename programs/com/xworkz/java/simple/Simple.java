6fcc62a8a4796e33d848dc0937b7d65c3b728832
acer <dyamannahadapad004@gmail.com>
2022-08-12 21:10:56 +0530

    12/08/Integer Const

--- programs/com/xworkz/java/simple/Simple.java
+++ programs/com/xworkz/java/simple/Simple.java
@@ -4,8 +4,8 @@ public class Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str="Dyamanna Hadapad";
-		System.out.println(str+" "+"length of words"+str.length());
+		String str = "Dyamanna Hadapad";
+		System.out.println(str + " " + "length of words" + str.length());
 
 	}
 
